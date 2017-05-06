b9b78d57283ec5d0ca13377bf040f52bfa8ab0a8
Blake Curry <blake.curry@gmx.us>
2017-05-06 18:23:10 -0400

    Changed bot to dynamicly find modules instead of having them hard coded.

--- bot.py
+++ bot.py
@@ -4,52 +4,30 @@
 import os
 import sys
 import asyncio
+import importlib
+import signal
 
 import discord
 from discord.enums import ChannelType
 from discord.ext import commands
 
 sys.path.insert(0, "lib")
-bot = commands.Bot(command_prefix='!', description="An agreeable bot.")
 
 
-@bot.event
-async def on_ready():
-    print('Logged in as %s(%s)' % (bot.user.name, bot.user.id))
-    print('------')
 
-#@bot.listen('on_message')
-async def on_message(message):
-    if(message.author.id != bot.user.id):
-        if(not '!' in message.content):
-            await bot.send_message(message.channel, 'I agree')
+
     
+blacklist = ["__pycache__", 'lib', 'data', '.', '..', '.git']
 
-import infodump.infodump
-infodump.infodump.setup(bot)
+def on_sigint(signal, frame):
+        print('We have been interrupted.')
+        sys.exit(0)
 
-import archiver.archiver
-archiver.archiver.setup(bot)
+signal.signal(signal.SIGINT, on_sigint)    
 
-import mimic.mimic
-mimic.mimic.setup(bot)
-
-import notifier.notifier
-notifier.notifier.setup(bot)
-
-import rgrcog.rgrcog
-rgrcog.rgrcog.setup(bot)
 
 
 def main():
-    import signal
-    
-    print("Starting YesMan")
-
-    def on_sigint(signal, frame):
-        print('We have been interrupted.')
-        sys.exit(0)
-    signal.signal(signal.SIGINT, on_sigint)    
     
     #make sure the config file exists
     if(not os.path.exists('config.py')):
@@ -58,6 +36,20 @@ def main():
             print("Put your client id and bot token from developer.discordapp.com in config.py")
             sys.exit(1)
     import config
+    
+    print("Starting YesMan")
+    bot = commands.Bot(command_prefix='!', description="An agreeable bot.")
+    
+    for f in os.scandir():
+        if f.is_dir() and f.name not in blacklist:
+            mod = importlib.import_module("%s.%s" % (f.name, f.name))
+            mod.setup(bot)
+
+    @bot.event
+    async def on_ready():
+        print('Logged in as %s(%s)' % (bot.user.name, bot.user.id))
+        print('------')    
+    
     print('Enter the url below into your browser to be prompted to add the bot to a server you have manage permissions on.')
     print('https://discordapp.com/oauth2/authorize?client_id={}&scope=bot'.format(config.app_id))
     bot.run(config.token)
