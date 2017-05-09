68542c0048a2d35ad920f73d80105033a3804ec0
Blake Curry <blake.curry@gmx.us>
2017-05-08 21:04:55 -0400

    added debugging option to bot.py

--- bot.py
+++ bot.py
@@ -6,6 +6,8 @@ import sys
 import asyncio
 import importlib
 import signal
+import logging
+import argparse
 
 import discord
 from discord.enums import ChannelType
@@ -25,11 +27,28 @@ def on_sigint(signal, frame):
 
 signal.signal(signal.SIGINT, on_sigint)    
 
+def set_logger():
+    "logger making function adapted from Red's."
+    logger = logging.getLogger("bot.py")
+    logger.setLevel(logging.INFO)
 
+    red_format = logging.Formatter(
+        '%(asctime)s %(levelname)s %(module)s %(funcName)s %(lineno)d: '
+        '%(message)s',
+        datefmt="[%d/%m/%Y %H:%M]")
 
-def main():
+    stdout_handler = logging.StreamHandler(sys.stdout)
+    stdout_handler.setFormatter(red_format)
     
-    #make sure the config file exists
+    stdout_handler.setLevel(logging.DEBUG)
+    
+    logger.addHandler(stdout_handler)
+
+    return logger
+
+def main(args):
+    
+    #make sure the config file exists    
     if(not os.path.exists('config.py')):
         with open('config.py', 'w') as config:
             config.write("app_id = ''\ntoken = ''")
@@ -39,6 +58,15 @@ def main():
     
     print("Starting YesMan")
     bot = commands.Bot(command_prefix='!', description="An agreeable bot.")
+    
+    #attach logger
+    bot.logger = set_logger()
+    
+    #set debug
+    if args.debug:
+        bot.logger.setLevel(logging.DEBUG)
+        bot.loop.set_debug(True)
+    
     
     for f in os.scandir():
         if f.is_dir() and f.name not in blacklist:
@@ -56,4 +84,12 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('--debug', action='store_true')
+    args = parser.parse_args()
+    if args.debug:
+        import cProfile as profile
+        logging.basicConfig(level=logging.DEBUG)
+        profile.run('main(args)', "restats")
+    else:
+        main(args)
